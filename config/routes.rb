3bf45ef85b8b836e4d4f3aa5c5e47e464d9006fd
kou.mizukami <kou0830fm@outlook.jp>
2020-03-18 08:34:26 +0000

    edit: routes.rb users_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'toppages#home'
   get '/signup',   to: 'users#new'
-  # get  '/help',    to: 'static_pages#help'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
