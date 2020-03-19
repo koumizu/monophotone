30c8e27786126d47bb29027ad30f1088f359d2df
kou.mizukami <kou0830fm@outlook.jp>
2020-03-19 10:42:13 +0000

    edit: テスト環境でのドメインホストの設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '1739981407374fa097c1606ee461d871.vfs.cloud9.us-east-1.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
