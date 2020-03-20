47746985395f3d36a5b9d7ffbe58094b684fe0d1
kou.mizukami <kou0830fm@outlook.jp>
2020-03-20 08:18:11 +0000

    edit: pictur属性にvalidates追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :picture, presence: true
 end
