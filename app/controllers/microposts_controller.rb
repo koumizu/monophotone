d1edd9938640e9a3201b37127abf15b160cd22e0
kou.mizukami <kou0830fm@outlook.jp>
2020-03-21 06:26:26 +0000

    edit: logged_in_userメソッド追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,15 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:show, :new, :create, :destroy]
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
 end
