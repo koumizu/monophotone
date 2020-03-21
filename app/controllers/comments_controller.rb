ae489ff10fecebc3632db55a5ced186ed8d303ad
kou.mizukami <kou0830fm@outlook.jp>
2020-03-21 10:09:17 +0000

    add: create,destroyアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :logged_in_user
+  
+  def create
+    @micropost = Micropost.find(params[:micropost_id]) 
+    @comment = @micropost.comments.build(comment_params) 
+    @comment.user_id = current_user.id 
+    if @comment.save
+      render :index
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :index
+    end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content, :micropost_id, :user_id)
+  end
 end
