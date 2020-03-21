3a5596212770957bc94f41a6193a96b471a1d7ba
kou.mizukami <kou0830fm@outlook.jp>
2020-03-21 09:22:05 +0000

    edit: create,destroyアクション追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :logged_in_user
+  
+  def create
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.like(@micropost)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+  end
+  
+  def destroy
+    @micropost = Favorite.find(params[:id]).micropost
+    if current_user.like?(@micropost)
+      current_user.unlike(@micropost)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
+  end
 end
