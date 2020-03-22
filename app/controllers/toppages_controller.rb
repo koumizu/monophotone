ea82ab8c4a9e6a44c5c0c661c8146d6ef66b41a6
kou.mizukami <kou0830fm@outlook.jp>
2020-03-22 05:42:55 +0000

    edit: new_guestメソッド編集

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -6,7 +6,7 @@ class ToppagesController < ApplicationController
   end
   
   def new_guest
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(name: "guest", email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
     log_in(user)
