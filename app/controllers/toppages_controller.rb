88091d5bce3990cf6b331f768d85d05d2ce83d25
kou.mizukami <kou0830fm@outlook.jp>
2020-03-22 05:39:52 +0000

    edit: new_guestメソッド追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,4 +4,12 @@ class ToppagesController < ApplicationController
       @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
     end
   end
+  
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    log_in(user)
+    redirect_to root_path
+  end
 end
